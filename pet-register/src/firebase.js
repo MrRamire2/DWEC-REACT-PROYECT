6fd2e26f619c4f395c783773a3b5d7cef686257b
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-02-17 21:14:52 +0100

    respaldo

---
+++ pet-register/src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBh_cYoq-2BIeXcm0mmcrGSEEyHc6XuyJA",
+    authDomain: "petregisterdb.firebaseapp.com",
+    projectId: "petregisterdb",
+    storageBucket: "petregisterdb.firebasestorage.app",
+    messagingSenderId: "958590442273",
+    appId: "1:958590442273:web:96a52516c7d8a0c2e67c60",
+    measurementId: "G-NE4PBWJYG0"
+  };
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export default firebase;
